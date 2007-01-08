f8a39c55aad9765340fd8dc2ff73573ba38c0400
nicolas.boulay <nicolas.boulay@4a5454c4-a219-0410-82dd-e5acffe59b65>
2007-01-08 20:47:07 +0000

    1er transfert !

--- ImageLab/nico/video.cpp
+++ ImageLab/nico/video.cpp
@@ -3,25 +3,76 @@
 
 #include<stdio.h> //perror()
 #include<fcntl.h> //O_RDONLY
-//#include </usr/src/linux/drivers/usb/media/pwc/pwc-ioctl.h>
-#include <linux/videodev.h> //drivers video4linux
 #include <sys/ioctl.h> // system d'appel ioctl
 
-
-//#include<stdlib.h> //open()
 #include <sys/types.h>
 #include <sys/stat.h>
 
-//using namespace std;
+#include <errno.h>
+#include <sys/mman.h> //mmap
 
-video_t::video_t(const string &  s)
+using namespace cimg_library;
+
+video_t::video_t(const std::string &  s)
+
 {
+  height = 120;
+  width  = 160;
+  framerate = 60;
+  img[0]= new CImg<unsigned char>(height,width,1,3,100);
+  img[1]= new CImg<unsigned char>(height,width,1,3,0);
+
   video_t::_open(s);
 } 
 
+video_t::~video_t()
+
+{
+  close(fd);
+}
+
 void video_t::_open(const string & s) 
 {
-  fd = open(s.c_str(), O_RDONLY);
+  struct stat st; 
+
+  if (-1 == stat (s.c_str(), &st)) {
+    fprintf (stderr, "Cannot identify '%s': %d, %s\n",
+	     s.c_str(), errno, strerror (errno));
+    exit (EXIT_FAILURE);
+  }
+
+  if (!S_ISCHR (st.st_mode)) {
+    fprintf (stderr, "%s is no device\n", s.c_str());
+    exit (EXIT_FAILURE);
+  }
+
+  fd = open (s.c_str(), O_RDWR /* required */ | O_NONBLOCK, 0);
+
+  cout << "open device :" << s << endl;
+
+  if (-1 == fd) {
+    fprintf (stderr, "Cannot open '%s': %d, %s\n",
+	     s.c_str(), errno, strerror (errno));
+    exit (EXIT_FAILURE);
+  }
+
+  /* Get device capabilities */
+  if (ioctl (fd, VIDIOCGCAP, &vcap) < 0) {
+    perror ("VIDIOCGCAP");
+    exit (EXIT_FAILURE);
+  }
+
+  printf ("Video Capture Device Name : %s\n", vcap.name);
+
+  /* Mmap frame-buffer */
+  framebuf = (unsigned char*)
+    mmap(0, width * height * 3, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  print_parameter();
+  if ((unsigned char *)-1 == (unsigned char *)framebuf) {
+    perror ("mmap");
+    exit (EXIT_FAILURE);
+  }
+
 }
 
 int video_t::set (int width_, int height_, int framerate_,
@@ -94,4 +145,150 @@ void video_t::fetch()
   //if(ioctl(fd, VIDIOCPWCGAGC, &agc)<0){
   //  perror("video_t, agc getting error");
   //}
+}
+
+void video_t::print_parameter(void)
+{
+  printf("%ix%i %ifps\n", height, width, framerate);
+
+}
+
+CImg<unsigned char> & video_t::grab_frame(void) 
+{
+  mm.frame  = 0;
+  mm.height = height;
+  mm.width  = width;
+
+  mm.format = VIDEO_PALETTE_YUV420P;
+
+  /* Get frame */
+  if (ioctl(fd, VIDIOCMCAPTURE, &mm) < 0) {
+    perror ("VIDIOCMCAPTURE");
+    exit (EXIT_FAILURE);
+  }
+
+  /* Wait frame to be completed */
+  if (ioctl(fd, VIDIOCSYNC, &mm.frame) < 0) {
+    perror ("VIDIOCSYNC");
+    exit (EXIT_FAILURE);
+    }
+
+  //unsigned char * rgb 
+  //  = (unsigned char *)malloc(width*height*3*sizeof(unsigned char));
+  yuv420P_to_rgb24(framebuf,*img[0]);
+
+  //  img[0]->assign(rgb,height,width);
+  //free (rgb);
+  return * img[0];
+}
+
+
+/* Go from RGB (red first) to 4:2:0 planar.
+ * Note: this requires decimation of the U/V space by 2 in both directions
+ * Also, a matrix multiply would be QUITE convenient...
+
+   This is the matrix:
+     (Y )   ( 77  150   29)   (R)
+     (Cb) = (-43  -85  128) * (G)
+     (Cr)   (128 -107  -21)   (B)
+ */
+
+
+//fonction de conversion yuv -> rgb pour affichage
+void video_t::yuv420P_to_rgb24(
+			       unsigned char * yuv, 
+			       unsigned char * rgb, 
+			       const unsigned width, 
+			       const  unsigned height)
+{
+  unsigned int y,u,v,w,h;
+
+ for(w=0;w<width;w=w+1)
+   for(h=0; h<height ;h=h+1)
+   {
+     y=w+h*width;
+     u=width*height+(w/2+ (h/2)*(width/2));
+     v=u+(width/2)*(height/2);
+
+     rgb[3*w+h*3*width]=abs(yuv[y] + 1.4075*(yuv[v]-128));
+     rgb[1+3*w+h*3*width]=abs(yuv[y]- 0.344*(yuv[u]-128) - 0.7169*(yuv[v]-128));
+     rgb[2+3*w+h*3*width]=abs(yuv[y]+ 1.779*(yuv[u]-128));
+
+   }
+
+}
+/*
+
+C = Y - 16
+D = U - 128
+E = V - 128
+
+Using the previous coefficients and noting that clip() denotes clipping 
+a value to the range of 0 to 255, the following formulas provide the 
+conversion from YUV to RGB:
+
+R = clip(( 298 * C           + 409 * E + 128) >> 8)
+G = clip(( 298 * C - 100 * D - 208 * E + 128) >> 8)
+B = clip(( 298 * C + 516 * D           + 128) >> 8)
+*/
+
+inline unsigned int video_t::clip8(unsigned int i)
+{
+  if (i >= 255)
+    return 255;
+  return i;
+}
+
+inline unsigned char * video_t::YUV444toRGB888(unsigned int Y, 
+						     unsigned int U, 
+						     unsigned int V,
+						     unsigned char ret[3])
+{
+  int C, D, E;
+  C = Y - 16;
+  D = U - 128;
+  E = V - 128;
+
+  ret[0] = clip8(( 298 * C           + 409 * E + 128) >> 8);
+  ret[1] = clip8(( 298 * C - 100 * D - 208 * E + 128) >> 8);
+  ret[2] = clip8(( 298 * C + 516 * D           + 128) >> 8);
+
+  return ret;
+}
+
+inline unsigned char * video_t::yuv420p_get(
+					  unsigned char * yuv,
+					  unsigned char ret[3],
+					  unsigned w,
+					  unsigned h)
+
+{
+  unsigned YArraySize = height * width;
+  ret[0] = yuv[h * width + w];
+  ret[1] = yuv[(h/2) * (width/2) + w/2 + YArraySize];
+  ret[2] = yuv[(h/2) * (width/2) + w/2 + YArraySize + YArraySize/4];
+  
+  return ret;
+}
+
+
+/*
+B = 1.164(Y - 16)                   + 2.018(U - 128)
+G = 1.164(Y - 16) - 0.813(V - 128) - 0.391(U - 128)
+R = 1.164(Y - 16) + 1.596(V - 128)*/
+
+//fonction de conversion yuv -> rgb pour affichage
+void video_t::yuv420P_to_rgb24(unsigned char * yuv, 
+			       CImg<unsigned char> & rgb)
+{
+  cimg_mapXY(rgb,w,h) 
+    { 
+      unsigned char YUV[3];
+      yuv420p_get(yuv,YUV,h,w);
+      unsigned char RGB[3];
+      YUV444toRGB888(YUV[0],YUV[1],YUV[2],RGB);
+      rgb(w,h,0)=RGB[0];
+      rgb(w,h,1)=RGB[1];
+      rgb(w,h,2)=RGB[2];
+    }
 }
--- ImageLab/nico/video.hpp
+++ ImageLab/nico/video.hpp
@@ -1,23 +1,25 @@
 #ifndef VIDEO_CPP
 #define VIDEO_CPP
+#include "CImg.h"
 #include <string>
 #include <linux/types.h> // miss insinde videodev2 !!
-#include <linux/videodev2.h>
-
+#include <linux/videodev.h> //drivers video4linux 1.0 deprecated
+#include <linux/videodev2.h> //drivers video4linux 2.0
 using namespace std; 
 
 class video_t{
 public:
-  video_t(const string &  s);
-  video_t(void) : file_name(NULL), fd(-1) {};
+  video_t(const std::string &  s);
+  ~video_t();
   int set (int width_, int height_, int framerate_,
        int brightness_,  int colour_, int contrast_,
        int whiteness_, int palette_, int agc_);
   void commit();
   void fetch(void);
-
-  int width;
-  int height;
+  void print_parameter(void);
+  cimg_library::CImg<unsigned char> & grab_frame(void); 
+  unsigned int width;
+  unsigned int height;
   int framerate;
   int brightness;
   int colour;
@@ -29,7 +31,30 @@ public:
 private:
   string file_name;
   int fd;
+
+  // V4L 1.0
+  unsigned char *framebuf;
+  struct video_capability vcap;
+  struct video_channel    vc;
+  struct video_mmap       mm;
+  cimg_library::CImg<unsigned char> * img[2];
   void _open(const string & s);
+  inline unsigned int clip8(unsigned int i);
+  inline unsigned char * YUV444toRGB888(unsigned int Y,
+					      unsigned int U, 
+					      unsigned int V,
+					      unsigned char ret[3]);
+  inline unsigned char * yuv420p_get(
+					   unsigned char * yuv,
+					   unsigned char ret[3],
+					   unsigned w,
+					   unsigned h);
+  void yuv420P_to_rgb24(unsigned char * yuv, 
+			unsigned char * rgb,
+			const unsigned width,
+			const  unsigned height);
+  void yuv420P_to_rgb24(unsigned char * yuv, 
+			cimg_library::CImg<unsigned char> & rgb);
 };
 
 #endif
