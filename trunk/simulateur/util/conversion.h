5b6ca1679857e00cddcc03543b85d6a26426a37b
pierre.feyzeau <pierre.feyzeau@4a5454c4-a219-0410-82dd-e5acffe59b65>
2007-03-20 22:18:34 +0000

    algoPlanifTraj manquant

---
+++ trunk/simulateur/util/conversion.h
@@ -0,0 +1,16 @@
+#ifndef ConversionH
+#define ConversionH
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+string int2String    (int i);
+string double2String (double i);
+int    string2Int    (string str);
+double string2Double (string str);
+
+
+#endif
