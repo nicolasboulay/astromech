1e18db1f95e5bf832a6061cc2ae4e17d5b04ea26
nicolas.boulay <nicolas.boulay@4a5454c4-a219-0410-82dd-e5acffe59b65>
2007-05-05 23:40:13 +0000

    add missing file

---
+++ trunk/IA/src/task_rt.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include "manager.h"
+#include "trame_out.h"
+#include "trame_in.h"
+#include "serial.h"
+#include "task_rt.h"
+
+using namespace std;
+
+ void task_rt_t::run2()
+ {
+
+//     serial_t com(device);
+//     trame_binary_t tbin;
+//     trame_binary_t tbout;
+//     trame_out_t out;
+//     trame_in_t in;
+
+//     out.led1_jaune = 1;
+//     out.led1_vert = 0;
+//     out.led1_orange = 1;
+//     out.led2_rouge=0;
+//     out.led2_orange=1;
+//     out.led2_jaune=1;
+//     out.led3_vert=1;
+//     out.led3_jaune=1;
+//     out.led3_orange=0;
+//     out.led3_rouge=0;
+//         int j=0;
+
+//     manager_t manager;
+
+//     out=manager.execute(in);
+//     //    out.print();
+
+//   while(1){
+    
+//     cout << "---------------------------------------------------\n";
+//     //    tbout[12]=i++;
+//     out.led1_jaune  ^= 1;
+//     out.led1_vert   ^= 1;
+//     out.led1_orange ^= 1;
+//     out.led2_rouge  ^= 1;
+//     out.led2_orange ^= 1;
+//     out.led2_jaune  ^= 1;
+//     out.led3_vert   ^= 1;
+//     out.led3_jaune  ^= 1;
+//     out.led3_orange ^= 1;
+//     out.led3_rouge  ^= 1;
+
+
+//     out.serialise(tbout);
+//     tbout.inc_num_trame();
+//     tbout.gen_valid_pc_pic_paquet();
+//     tbout.gen_valid_trame();
+//     emit newTrameOut(tbout);
+//     tbout.dump_on_file("trame_from_pc.txt");
+
+//     cout << "out:";
+//     tbout.print();
+//     cout << endl;
+//     cout << "valide trame ?" << tbout.validation_trame() << endl;
+//     cout << "valide pic ?" << tbout.validation_paquet_pc_pic1() << endl;
+
+
+//     com.exchange(tbin,tbout);
+//     emit newTrameIn(tbin);
+//     cout << "in:";
+//     tbin.print_from_pic();
+//     cout << endl;
+//     cout << "valide trame ?" << tbin.validation_trame() << endl;
+//     cout << "validation ?" << tbin.validation() << endl;
+//     tbin.dump_on_file("trame_from_pic.txt");
+
+//     sleep(1);
+//   }
+  
+ }
+
+void task_rt_t::run()
+{
+  serial_t comRS232(device);
+  trame_binary_t tbin;
+  trame_binary_t tbout;
+  trame_out_t out;
+  trame_in_t in;    
+  manager_t manager;
+
+  sleep(1);
+   emit newTreeIn(in); // for debugging without the robot...
+
+  forever{
+    out=manager.execute(in); 
+    out.serialise(tbout);
+    tbout.gen_valid_header_trailer();
+    tbout.dump_on_file("trame_from_pc.txt");
+    emit newTrameOut(tbout);
+    emit newTreeOut(out);
+    comRS232.exchange(tbin,tbout);
+    emit newTrameIn(tbin);
+    if(tbin.validation()){
+      // deserialisation uniquement si la trame est valide.
+      // sinon on conserve la donnée précédente
+      // TODO:Il faudrait mettre qq choses dans l'état du robot
+      in.deserialise(tbin);
+      emit newTreeIn(in);
+    }
+    tbin.dump_on_file("trame_from_pic.txt");
+    sleep(1);
+  }
+}
---
+++ trunk/IA/src/task_rt.h
@@ -0,0 +1,35 @@
+#ifndef TASK_RT_H
+#define TASK_RT_H
+#include <QThread>
+#include <QVector>
+#include <QObject>
+#include <string.h>
+#include "trame_out.h"
+#include "trame_in.h"
+
+// Main task.
+// This run in a thread to leave the main thread for gui
+//  
+
+class task_rt_t : public QThread
+{    
+  Q_OBJECT
+
+public:
+  void run();
+  void run2();
+  task_rt_t(const char* _device) {strcpy(device,_device);}
+
+signals:
+  void newTrameOut(QVector<unsigned char> tab);
+  void newTrameIn(QVector<unsigned char> tab);
+  void newTreeOut(const trame_out_t & out);
+  void newTreeIn(const trame_in_t & in);
+  void newTrameC(int i, int j,int val);
+private:
+  char device[32];
+  int j;
+
+};
+
+#endif
